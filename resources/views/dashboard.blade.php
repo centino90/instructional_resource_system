273106ce98151520d476e1e6b349960fed08ff55
RockyCalo <rockycalo89@gmail.com>
2021-09-19 14:41:20 +0800

    gi ilisan nako ang dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="breadcrumb">
         <x-breadcrumb>
-            <li class="breadcrumb-item invisible">Dashboard Calo</li>
+            <li class="breadcrumb-item invisible">Dashboard</li>
         </x-breadcrumb>
     </x-slot>
 
